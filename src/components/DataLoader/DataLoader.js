6b0fea4d56838dfdc1db6daa0982dc37efcdae4f
Mu'tasim Billah <mutasim.wre@gmail.com>
2018-08-19 15:28:23 +0600

    Initial commit

---
+++ src/components/DataLoader/DataLoader.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DataLoader extends Component {
+  render() {
+    return (
+      <div className="DataLoader">
+        <p>DataLoader Component</p>
+      </div>
+    )
+  }
+}
