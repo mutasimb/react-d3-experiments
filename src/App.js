55acae6436b26274326659d53a01bebb76372725
Mu'tasim Billah <mutasim.wre@gmail.com>
2018-08-20 03:37:31 +0600

    Data loader component with dropzone + sample formatted data downloader

--- src/App.js
+++ src/App.js
@@ -5,17 +5,29 @@ import css from './App.css';
 import DataLoader from './components/DataLoader/DataLoader'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+
+  dataHandler = data => {
+    this.setState({ data });
+  }
+
   render() {
+    let { data } = this.state;
+
     return (
       <div className={ css.App }>
         <header className={ css.AppHeader }>
           <img src={logo} className={ css.AppLogo } alt="logo" />
           <h1 className={ css.AppTitle }>Welcome to React</h1>
         </header>
-        <DataLoader />
-        <p className={ css.AppIntro }>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className={ css.AppIntro }>{ JSON.stringify(data[0], null, 2) }</p>
+        { data.length === 0 ? <DataLoader onDataLoading={ this.dataHandler } /> : null }
       </div>
     );
   }
--- src/components/DataLoader/DataLoader.js
+++ src/components/DataLoader/DataLoader.js
@@ -1,12 +1,131 @@
 import React, { Component } from 'react'
+import ReactDropzone from 'react-dropzone'
+import { csv } from 'd3-request'
+import { format } from 'd3-format'
+import classnames from 'classnames'
 import css from './DataLoader.css'
 
+console.log({ css });
+
 export default class DataLoader extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSampleDownload: false,
+      validDataLoaded: false,
+      message: '',
+      error: false
+    }
+  }
+
+  setErrorMessage = (message, duration = 2500) => {
+
+    this.setState({ message, error: true });
+    setTimeout(() => {
+      this.setState({ message: '', error: false });
+    }, duration);
+
+  }
+
+  setSuccessMessage = message => {
+
+    this.setState({ message, validDataLoaded: true });
+
+  }
+
+  readCsv = file => {
+    csv(file, data => {
+
+      let requiredColumns = ["date", "site_id", "station_id", "rainfall_mm"];
+      let missingColumns = requiredColumns.filter(col => Object.keys(data[0]).indexOf(col) === -1);
+
+      if(missingColumns.length > 0) {
+        this.setState({ showSampleDownload: true });
+
+        this.setErrorMessage(
+          `The loaded .csv file isn't properly formatted. The following columns are missing from the data table: ${
+            missingColumns.join(", ")
+          }.`,
+          6000
+        );
+      
+      } else {
+
+        this.setSuccessMessage("Success!");
+        setTimeout(() => {
+
+          this.props.onDataLoading(
+            data.map(row => ({
+              date: row.date,
+              site_station: `${row.site_id}-${ format("02")(row.station_id) }`,
+              rainfall_mm: row.rainfall_mm
+            }))
+          );
+
+        }, 1500);
+      
+      }
+
+    });
+  }
+
+  onDrop = files => {
+
+    if(files.length > 1) {
+      this.setErrorMessage("Please do not load multiple files!");
+    } else if(files[0].name.indexOf(".csv") === -1 || files[0].type !== "text/csv") {
+      this.setErrorMessage("Please load a valid .csv file");
+    } else {
+
+      let reader = new FileReader();
+      reader.onloadend = event => {
+        let dataUrl = event.target.result;
+        this.readCsv(dataUrl);
+      }
+
+      reader.readAsDataURL(files[0]);
+    
+    }
+    
+  }
+
   render() {
-    return (
-      <div className={ css.DataLoader }>
-        <p>DataLoader Component</p>
-      </div>
-    )
+    let { message, error, validDataLoaded } = this.state;
+
+    return <div className={ css.DataLoader }>
+      <ReactDropzone
+        className={ classnames(css.DropZone, {
+          [`${ css.Error }`]: error,
+          [`${ css.Success }`]: validDataLoaded
+        }) }
+        activeClassName={ css.Active }
+        onDrop={ this.onDrop }
+        style={{
+          position: "fixed",
+          top: "0",
+          bottom: "0",
+          left: "0",
+          right: "0",
+          padding: "30px",
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#f9fbfc"
+        }}
+      >
+        <div className={ css.DottedBox } >
+          <span className={ classnames("lnr", "lnr-upload", css.LnrUpload) }></span>
+          { message === '' ? <p>Drag here or <span>browse</span> to upload</p> : <p>{message}</p> }
+        </div>
+      </ReactDropzone>
+      { this.state.showSampleDownload ? <a className={ css.SampleDownload } href="data/sample_data.csv" download>
+        <p>Sample formatted .csv file</p>
+      </a> : null}
+    </div>;
   }
 }
